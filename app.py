901ab9dad50ed269dfe25feb3417aad338f11f2f
mohit-iitbh <150344621+mohit-iitbh@users.noreply.github.com>
2024-05-01 20:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import subprocess
 import sys
 import streamlit as st
 import run_with_webcam
+import time
+
+st.title("Gaze Predictor")
 
 def start_capture():
     subprocess.run([f"{sys.executable}", "run_with_webcam.py"])
 
-if st.button("Start Capturing"):
+if st.button("Start Capturing Gaze"):
     st.write(start_capture())
 
